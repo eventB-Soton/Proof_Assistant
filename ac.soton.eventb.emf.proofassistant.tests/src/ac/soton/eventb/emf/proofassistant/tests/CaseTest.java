011f6539140e0d86a655656a83c43fd4b3938cbd
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2019-05-12 15:48:39 +0100

    All plug-ins: Update License to Eclispe v2.0

--- ac.soton.eventb.emf.proofassistant.tests/src/ac/soton/eventb/emf/proofassistant/tests/CaseTest.java
+++ ac.soton.eventb.emf.proofassistant.tests/src/ac/soton/eventb/emf/proofassistant/tests/CaseTest.java
@@ -1,13 +1,14 @@
-/**
+/*******************************************************************************
  * Copyright (c) 2019 University of Southampton.
- * 
- * All rights reserved. This program and the accompanying materials  are made
- * available under the terms of the Eclipse Public License v1.0 which accompanies this 
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
  *
- * $Id$
- */
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     University of Southampton - initial API and implementation
+ *******************************************************************************/
 package ac.soton.eventb.emf.proofassistant.tests;
 
 import ac.soton.eventb.emf.proofassistant.Case;
